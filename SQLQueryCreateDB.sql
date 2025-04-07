2fb69f0743ba51cdba36045fdb727f8f6a9440a9
gaabi <gaabi26092005@gmail.com>
2025-04-07 17:56:20 -0300

    Nuevo proyecto "CapaPresentacionTienda". Bundles modificados. Nueva librería Font.Awesome. Nueva Master Page.

---
+++ SQLQueryCreateDB.sql
@@ -0,0 +1,111 @@
+create database DBCARRITO
+go
+
+use dbcarrito
+go
+
+create table CATEGORIA(
+IdCategoria int primary key identity,
+Descripcion varchar(100),
+Activo bit default 1,
+FechaRegistro datetime default getdate()
+)
+go
+
+create table MARCA(
+IdMarca int primary key identity,
+Descripcion varchar(100),
+Activo bit default 1,
+FechaRegistro datetime default getdate()
+)
+go
+
+create table PRODUCTO(
+IdProducto int primary key identity,
+Nombre varchar(500),
+Descripcion varchar(500),
+IdMarca int references MARCA(IdMarca),
+IdCategoria int references CATEGORIA(IdCategoria),
+Precio decimal (10,2) default 0,
+Stock int,
+RutaImagen varchar(100),
+NombreImagen varchar(100),
+Activo bit default 1,
+FechaRegistro datetime default getdate()
+)
+go
+
+create table CLIENTE(
+IdCliente int primary key identity,
+Nombres varchar(100),
+Apellidos varchar(100),
+Correo varchar(100),
+Clave varchar (150),
+Reestablecer bit default 0,
+FechaRegistro datetime default getdate()
+)
+go
+
+create table CARRITO(
+IdCarrito int primary key identity,
+IdCliente int references CLIENTE(IdCliente),
+IdProducto int references PRODUCTO(IdProducto),
+Cantidad int
+)
+go
+
+create table VENTA(
+IdVenta int primary key identity,
+IdCliente int references CLIENTE(IdCliente),
+TotalProducto int,
+MontoTotal decimal(10,2),
+Contacto varchar(50),
+IdDistrito varchar(10),
+Telefono varchar(50),
+Direccion varchar(500),
+IdTransaccion varchar(50),
+FechaVenta datetime default getdate()
+)
+go
+
+create table DETALLE_VENTA(
+IdDetalleVenta int primary key identity,
+IdVenta int references VENTA(IdVenta),
+IdProducto int references PRODUCTO(IdProducto),
+Cantidad int,
+Total decimal(10,2)
+)
+go
+
+create table USUARIO(
+IdUsuario int primary key identity,
+Nombres varchar(100),
+Apellidos varchar(100),
+Correo varchar(100),
+Clave varchar(150),
+Reestablecer bit default 1,
+Activo bit default 1,
+FechaRegistro datetime default getdate()
+)
+go
+
+create table DEPARTAMENTO(
+IdDepartamento varchar(2) NOT NULL,
+Descripcion varchar(45) NOT NULL
+)
+go
+
+create table PROVINCIA(
+IdProvincia varchar(4) NOT NULL,
+Descripcion varchar(45) NOT NULL,
+IdDepartamento varchar(2) NOT NULL
+)
+go
+
+create table DISTRITO(
+IdDistrito varchar(6) NOT NULL,
+Descripcion varchar(45) NOT NULL,
+IdProvincia varchar(4) NOT NULL,
+IdDepartamento varchar(2) NOT NULL
+)
+go
