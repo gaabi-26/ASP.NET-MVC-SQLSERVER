5a36b115364d06216b28eedcae0435ee274ba298
gaabi <gaabi26092005@gmail.com>
2025-04-07 19:54:32 -0300

    Creacion de clases con sus respectivas propiedades en "CapaEntidad".

---
+++ CapaEntidad/Provincia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Provincia
+    {
+        public int IdProvincion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
