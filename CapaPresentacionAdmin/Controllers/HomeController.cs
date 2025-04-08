0287028a47480fd6d5a22c78a197e74c00bd6d98
gaabi <gaabi26092005@gmail.com>
2025-04-08 12:58:26 -0300

    Nuevo controlador "MantenedorController". Nuevas vistas. Cambios en los href de la master page.

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -12,19 +12,23 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Usuarios()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
