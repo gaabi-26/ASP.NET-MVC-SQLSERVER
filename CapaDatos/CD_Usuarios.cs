d195bb80dcbd4fa4dbb2a2029eb07fc6369712f7
gaabi <gaabi26092005@gmail.com>
2025-04-10 21:18:42 -0300

    Agregamos clases CD_Usuarios y CN_Usuarios. Comenzamos con el metodo Listar().

--- CapaDatos/CD_Usuarios.cs
+++ CapaDatos/CD_Usuarios.cs
@@ -3,11 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
 namespace CapaDatos
 {
     public class CD_Usuarios
     {
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select IdUsuarios, Nombres, Apellidos, Correos, Clave, Restableceer, Activo from usuario";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
 
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuario()
+                            {
+                                IdUsuario = Convert.ToInt32(dr["IdUsuarios"]),
+                                Nombres = dr["Nombres"].ToString(),
+                                Apellidos = dr["Apellidos"].ToString(),
+                                Correo = dr["Correos"].ToString(),
+                                Clave = dr["Clave"].ToString(),
+                                Reestablecer = Convert.ToBoolean(dr["Restableceer"]),
+                                Activo = Convert.ToBoolean(dr["Activo"])
+                            });
+                        }
+                    }
+
+                }
+            }
+            catch 
+            {
+                lista = new List<Usuario>();
+            }
+            return lista;
+        }
     }
 }
---
+++ CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios objUsuario = new CD_Usuarios();
+        public List<Usuario> Listar()
+        {
+            return objUsuario.Listar();
+        }
+    }
+}
